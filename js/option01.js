3e0c25418fbb09e1dc5ee61aad3bba777046a116
Y-Ysss <y-ysss@outlook.com>
2017-08-03 20:52:07 +0900

    2.1.4

--- js/option01.js
+++ js/option01.js
@@ -1,8 +1,18 @@
 $(function() {
-
     $('#bodyMain').masonry({
         itemSelector: '.cardArea',
-        percentPosition: true
+        percentPosition: true,
         fitWidth: true
     });
-})
+    
+    $.getJSON('manifest.json').then(function(manifest) {
+        var str = '<div class="card_cntnt"><h4>Installed Version</h4>' + manifest.version + '</div>';
+        $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/releases/latest').then(function(data) {
+            if (manifest.version != data.name) {
+                str += '<h2>#New Release</h2><div class="card_cntnt"><h4>Version</h4>' + data.name + '</div><div class="card_cntnt"><h4>What\'s New</h4>' + data.body + '</div><div class="card_cntnt"><h4>URL</h4><a href="' + data.html_url + '">' + data.html_url + '</a></div>';
+                str = str.replace(/\r?\n/g, '<br>');
+            }
+            $('#gitInfo').append(str);
+        });
+    });
+});
