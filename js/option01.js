2fdafe4622db1362a7e0dc62110d35d5e9ef4b53
Y-Ysss <y-ysss@outlook.com>
2017-08-05 14:07:54 +0900

    2.1.7

--- js/option01.js
+++ js/option01.js
@@ -1,26 +1,48 @@
+// closure #test
+function addContent() {
+    var str = '';
+    return function(num, run) {
+        $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/commits').then(function(json) {
+            // console.log(json[num].commit.message + ',' + json[num].commit.author.date);
+            str += '<div class="card_cntnt"><h4>' + json[num].commit.message + '</h4>Date : ' + (json[num].commit.author.date).replace('T', '<br>Time : ').slice(0, -1) + ' (UTC)<br><a href="' + json[num].html_url + '">' + json[num].html_url + '</a></div>';
+            if (run)
+                $('#gitCommitsInfo').append(str);
+        });
+    }
+}
+
 $(function() {
-    $('#bodyMain').masonry({
+    var $msnry = $('#bodyMain').masonry({
         itemSelector: '.cardArea',
         percentPosition: true,
         fitWidth: true
     });
-    
+
     $.getJSON('manifest.json').then(function(manifest) {
         var str = '<div class="card_cntnt"><h4>Installed Version</h4>' + manifest.version + '</div>';
         $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/releases/latest').then(function(data) {
             if (manifest.version != data.name) {
-                str += '<h2>#New Release</h2><div class="card_cntnt"><h4>Version</h4>' + data.name + '</div><div class="card_cntnt"><h4>What\'s New</h4>' + data.body + '</div><div class="card_cntnt"><h4>URL</h4><a href="' + data.html_url + '">' + data.html_url + '</a></div>';
+                str += '<h2>#Latest Release</h2><div class="card_cntnt"><h4>Version</h4>' + data.name + '</div><div class="card_cntnt"><h4>What\'s New</h4>' + data.body + '</div><div class="card_cntnt"><h4>URL</h4><a href="' + data.html_url + '">' + data.html_url + '</a></div>';
                 str = str.replace(/\r?\n/g, '<br>');
             }
-            $('#gitInfo').append(str);
+            $('#ExtensionInfo').append(str);
+            $msnry.masonry('layout');
         });
     });
 
-    $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/commits').then(function(commitsList) {
-        console.log(commitsList[0].sha);
-        var lastCmmtManifest = 'https://raw.githubusercontent.com/Yoseatlly/HelloNewTab/' + commitsList[0].sha + '/manifest.json';
-        $.getJSON(lastCmmtManifest).then(function(manifest){
-            console.log(manifest.version);
-        });
-    });
+    var func = addContent();
+    func(0, 0);
+    func(1, 0);
+    func(2, 0);
+    func(3, 0);
+    func(4, 0);
+    func(5, 1);
+    $msnry.masonry('layout');
+
+    // $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/commits').then(function(commitsList) {
+    // var lastCmmtManifest = 'https://raw.githubusercontent.com/Yoseatlly/HelloNewTab/' + commitsList[0].sha + '/manifest.json';
+    // $.getJSON(lastCmmtManifest).then(function(manifest) {
+    //     console.log(manifest.version);
+    // });
+    // });
 });
