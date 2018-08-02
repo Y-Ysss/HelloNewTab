148f65cac09144254630cb12d6c128510b504de2
Y-Ysss <y-ysss@outlook.com>
2018-08-02 19:02:08 +0900

    5.3.0

--- js/newtab.js
+++ js/newtab.js
@@ -1,234 +1,246 @@
 let ini = new class {
-    constructor() {
-        //Default settings
-        this.settings = { "toggle": {"tgglIcon": -1, "tgglOpenTab": -1}, "text": {"txtScale": ""}, "radio": {"theme": "tmLight"}},
-        this.contentsData = "",
-        this.initData()
-    }
-    // save
-    saveData() {chrome.storage.local.set({ settings: this.settings })}
-    // load
-    initData() {
-        chrome.storage.local.get((a) => {
-            a.settings === undefined ? (this.saveData(), this.init()) : (this.settings = a.settings, this.contentsData = a.contentsData, this.init())
-        })
-    }
-    // called initData()
-    // Initialization
-    init() {
-        this.addContents()
-        this.walkJson(this.settings)
-        // console.log(this.settings)
-    }
+  constructor() {
+    //Default settings
+    this.settings = {
+      "toggle": {"tgglIcon": -1,"tgglOpenTab": -1},
+      "text": {"txtScale": "","range": {"sliderLower": "0","sliderUpper": "0"}},
+      "radio": {"theme": "tmLight"}
+    },
+    this.contentsData = "",
+    this.initData()
+  }
+  // save
+  saveData() { chrome.storage.local.set({ settings: this.settings }) }
+  // load
+  initData() {
+    chrome.storage.local.get((a) => {
+      a.settings === undefined ? (this.saveData(), this.init()) : (this.settings = a.settings, this.contentsData = a.contentsData, this.init())
+    })
+  }
+  // called initData()
+  // Initialization
+  init() {
+    this.addContents()
+    this.walkJson(this.settings)
+    // console.log(this.settings)
+  }
 
-    // called init() 
-    addContents() {
-        this.funcMacy();
-        $('#bodyMain').append(this.contentsData);
-        rippleEffect();
-        this.contentsData = "";
-    }
+  // called init() 
+  addContents() {
+    this.funcMacy();
+    $('#bodyMain').append(this.contentsData);
+    rippleEffect();
+    this.contentsData = "";
+  }
 
-    // called addContents()
-    funcMacy() {
-        let mw = parseInt($('#bodyMain').width() / 190);
-        let macy = Macy({
-            container: '#bodyMain',
-            trueOrder: false,
-            waitForImages: true,
-            columns: mw,
-            margin: {x: 30, y: 15},
-            breakAt: {1200: 5, 990: 4, 780: 3, 620: 2, 430: 1}
-        });
-    }
+  // called addContents()
+  funcMacy() {
+    let mw = parseInt($('#bodyMain').width() / 190);
+    let macy = Macy({
+      container: '#bodyMain',
+      trueOrder: false,
+      waitForImages: true,
+      columns: mw,
+      margin: { x: 30, y: 15 },
+      breakAt: { 1200: 5, 990: 4, 780: 3, 620: 2, 430: 1 }
+    });
+  }
 
-    // called init() 
-    walkJson(data) {
-        for (var key in data) {
-            if (typeof data[key] === "object") {
-                this.walkJson(data[key])
-            } else {
-                this[key](data[key])
-            }
-        }
+  // called init() 
+  walkJson(data) {
+    for(var key in data) {
+      if(typeof data[key] === "object") {
+        this.walkJson(data[key])
+      } else {
+        this[key](data[key])
+      }
     }
-    // called walkJson()
-    tgglIcon(a) {
-        a === 1 ? $('.favicon').css('border-radius', '0%') : $('.favicon').css('border-radius', '50%');
-    }
-    // called walkJson()
-    tgglOpenTab(a) {
-        if (a === 1) { $('head').append('<base target="_blank">'); }
-    }
-    // called walkJson()
-    txtScale(a) {
-        if (!isNaN(a) && a !== '') { $('html').css('zoom', a + '%'); }
-    }
-    // called walkJson()
-    theme(a) {
-        $('head').append('<link id="ssTheme" rel="stylesheet" type="text/css" href="css/theme/' + a + '.css">');
-        $('input[name="theme"]').val([a]);
-    }
-
+  }
+  // called walkJson()
+  tgglIcon(a) {
+    a === 1 ? $('.favicon').css('border-radius', '0%') : $('.favicon').css('border-radius', '50%');
+  }
+  // called walkJson()
+  tgglOpenTab(a) {
+    if(a === 1) { $('head').append('<base target="_blank">'); }
+  }
+  // called walkJson()
+  txtScale(a) {
+    if(!isNaN(a) && a !== '') { $('html').css('zoom', a + '%'); }
+  }
+  // called walkJson()
+  theme(a) {
+    $('head').append('<link id="ssTheme" rel="stylesheet" type="text/css" href="css/theme/' + a + '.css">');
+    $('input[name="theme"]').val([a]);
+  }
+  sliderLower() {}
+  sliderUpper() {}
 }
 
 let ev = new class {
-    // data
-    constructor() {
-        this.systemLinkArea = 0;
-        this.searchArea = 0;
-        this.filter = 0;
-        this.floatMenu = {theme : 0, visible : 0}
+  // data
+  constructor() {
+    this.systemLinkArea = 0;
+    this.searchArea = 0;
+    this.filter = 0;
+    this.floatMenu = { theme: 0, visible: 0 }
+  }
+  moreMenu(a = this.systemLinkArea) {
+    this.modeFilter(a);
+    if(a === 0) { // visible
+      this.searchMenu(1);
+      this.selectTheme(1);
+      this.vsbltyMenu(1);
+      $('#systemLinkArea').css('width', '19rem');
+      this.systemLinkArea = 1;
+    } else { // hidden
+      $('#systemLinkArea').css('width', '4rem');
+      this.systemLinkArea = 0;
     }
-    moreMenu(a = this.systemLinkArea) {
-        this.modeFilter(a);
-        if (a === 0) { // visible
-            this.searchMenu(1);
-            this.selectTheme(1);
-            this.vsbltyMenu(1);
-            $('#systemLinkArea').css('width', '19rem');
-            this.systemLinkArea = 1;
-        } else { // hidden
-            $('#systemLinkArea').css('width', '4rem');
-            this.systemLinkArea = 0;
+  }
+
+  searchMenu(a = this.searchArea) {
+    if(a === 0) { // visible
+      this.moreMenu(1);
+      this.selectTheme(1);
+      this.vsbltyMenu(1);
+      $('#searchGroup').css('left', '4rem');
+      // $('#searchGroup').css('width', '30rem');
+      $('#searchMenu').addClass('bg-searchMenu');
+      $('#search').focus();
+      this.searchArea = 1;
+    } else { // hidden
+      $('#searchGroup').css('left', '-34rem');
+      // $('#searchGroup').css('width', '0rem');
+      $('#searchMenu').removeClass('bg-searchMenu');
+      $('#search').blur();
+      this.searchArea = 0
+      this.searchReset();
+    }
+  }
+
+  modeFilter(a = this.filter) {
+    const mF = $('#mFilter');
+    if(a === 0) {
+      mF.addClass('filter');
+      this.filter = 1;
+    } else {
+      mF.removeClass('filter');
+      this.filter = 0;
+    }
+  }
+
+  searchReset() {
+    $('#search').val("");
+    $('#searchReset').removeClass('searchResetView');
+    $('#searchResult').empty();
+  }
+
+  searchView() {
+    const words = $('#search').val();
+    chrome.bookmarks.search(words, function(results) {
+      let joinResult = "";
+      results.forEach(function(item) {
+        if(item.url) {
+          const title = item.title == "" ? item.url : item.title;
+          joinResult += '<a href="' + item.url + '"><img class="favicon" src="chrome://favicon/' + item.url + '">' + title + '</a>';
         }
-    }
+      });
+      $('#searchResult').append(joinResult + '<div id="resultNum">Search Result : ' + results.length + '</div>');
+    });
+    $('#searchResult').empty();
+  }
 
-    searchMenu(a = this.searchArea) {
-        if (a === 0) { // visible
-            this.moreMenu(1);
-            this.selectTheme(1);
-            this.vsbltyMenu(1);
-            $('#searchGroup').css('left', '4rem');
-            // $('#searchGroup').css('width', '30rem');
-            $('#searchMenu').addClass('bg-searchMenu');
-            $('#search').focus();
-            this.searchArea = 1;
-        } else { // hidden
-            $('#searchGroup').css('left', '-34rem');
-            // $('#searchGroup').css('width', '0rem');
-            $('#searchMenu').removeClass('bg-searchMenu');
-            $('#search').blur();
-            this.searchArea = 0
-            this.searchReset();
-        }
+  selectTheme(a = this.floatMenu.theme) {
+    if(a === 0) {
+      this.moreMenu(1);
+      this.vsbltyMenu(1);
+      $('#fmTheme').css({ margin: '-3rem 0 0 4rem', visibility: 'visible', opacity: '1' });
+      this.floatMenu.theme = 1;
+    } else {
+      $('#fmTheme').css({ margin: '-3rem 0 0 3rem', visibility: 'hidden', opacity: '0' });
+      this.floatMenu.theme = 0;
     }
+  }
 
-    modeFilter(a = this.filter) {
-        const mF = $('#mFilter');
-        if (a === 0) {
-            mF.addClass('filter');
-            this.filter = 1;
-        } else {
-            mF.removeClass('filter');
-            this.filter = 0;
-        }
-    }
+  applyTheme() {
+    ini.saveData();
+    location.reload();
+  }
 
-    searchReset() {
-        $('#search').val("");
-        $('#searchReset').removeClass('searchResetView');
-        $('#searchResult').empty();
+  vsbltyMenu(a = this.floatMenu.visible) {
+    // this.moreMenu();
+    if(a === 0) {
+      this.moreMenu(1);
+      this.selectTheme(1);
+      $('#fmVsblty').css({ margin: '-3rem 0 0 4rem', visibility: 'visible', opacity: '1' });
+      this.floatMenu.visible = 1;
+    } else {
+      $('#fmVsblty').css({ margin: '-3rem 0 0 3rem', visibility: 'hidden', opacity: '0' });
+      this.floatMenu.visible = 0;
     }
-
-    searchView() {
-        const words = $('#search').val();
-        chrome.bookmarks.search(words, function(results) {
-            let joinResult = "";
-            results.forEach(function(item) {
-                if (item.url) {
-                    const title = item.title == "" ? item.url : item.title;
-                    joinResult += '<a href="' + item.url + '"><img class="favicon" src="chrome://favicon/' + item.url + '">' + title + '</a>';
-                }
-            });
-            $('#searchResult').append(joinResult + '<div id="resultNum">Search Result : ' + results.length + '</div>');
-        });
-        $('#searchResult').empty();
-    }
-
-    selectTheme(a = this.floatMenu.theme) {
-        if(a === 0) {
-            this.moreMenu(1);
-            this.vsbltyMenu(1);
-            $('#fmTheme').css({margin: '-3rem 0 0 4rem', visibility:'visible', opacity:'1'});
-            this.floatMenu.theme = 1;
-        } else {
-            $('#fmTheme').css({margin: '-3rem 0 0 3rem', visibility:'hidden', opacity:'0'});
-            this.floatMenu.theme = 0;
-        }
-    }
-
-    applyTheme() {
-        ini.saveData();
-        location.reload();
-    }
-
-    vsbltyMenu(a = this.floatMenu.visible) {
-        // this.moreMenu();
-        if(a === 0) {
-            this.moreMenu(1);
-            this.selectTheme(1);
-            $('#fmVsblty').css({margin: '-3rem 0 0 4rem', visibility:'visible', opacity:'1'});
-            this.floatMenu.visible = 1;
-        } else {
-            $('#fmVsblty').css({margin: '-3rem 0 0 3rem', visibility:'hidden', opacity:'0'});
-            this.floatMenu.visible = 0;
-        }
-    }
+  }
 }
 
 deSVG('.faviconBig', true);
 
 $('#search').keyup(function(e) {
-    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-        ev.searchView();
-    }
-    this.value !== "" ? $('#searchReset').addClass('searchResetView') : $('#searchReset').removeClass('searchResetView');
+  if((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+    ev.searchView();
+  }
+  this.value !== "" ? $('#searchReset').addClass('searchResetView') : $('#searchReset').removeClass('searchResetView');
 });
 $(document).keydown(function(event) {
-    if (event.altKey && event.keyCode === 66) { // [ Alt + B ]
-        // if ($('#search').val() === '') {
-            ev.searchMenu(ev.searchArea);
-        // }
-    }
-    if (event.keyCode === 27 && $('#search').focus()) { // [ Esc ]
-        ev.searchMenu(1);
-    }
+  if(event.altKey && event.keyCode === 66) { // [ Alt + B ]
+    // if ($('#search').val() === '') {
+    ev.searchMenu(ev.searchArea);
+    // }
+  }
+  if(event.keyCode === 27 && $('#search').focus()) { // [ Esc ]
+    ev.searchMenu(1);
+  }
 });
 $(document).click(function(event) {
-    if ($(event.target).closest('#mFilter').length) {
-        ev.moreMenu(1);
-    }
+  if($(event.target).closest('#mFilter').length) {
+    ev.moreMenu(1);
+  }
 });
 
 $('.actionItems').click(function() {
-    ev[this.id]();
+  ev[this.id]();
 });
 $('.createTabLink').click(function() {
-    chrome.tabs.create({ url: $(this).attr('href') });
-    ev.moreMenu(1);
-    $('#mFilter').removeClass('filter');
+  chrome.tabs.create({ url: $(this).attr('href') });
+  ev.moreMenu(1);
+  $('#mFilter').removeClass('filter');
 });
 
 $('#tggl1').click(function() {
-    $(this).toggleClass('form_tggl_on');
-    if ($(this).hasClass('form_tggl_on')) {
-        $('.hideModule').css({visibility:'visible', opacity:'1'});
-    } else {
-        $('.hideModule').css({visibility:'hidden', opacity:'0'});
-    }
+  $(this).toggleClass('form_tggl_on');
+  if($(this).hasClass('form_tggl_on')) {
+    $('.hideModule').css({ visibility: 'visible', opacity: '1' });
+  } else {
+    $('.hideModule').css({ visibility: 'hidden', opacity: '0' });
+  }
 });
 
 $('input[type="radio"]').click(function() {
-    ini.settings.radio[$(this)[0].name] = $(this)[0].id;
+  ini.settings.radio[$(this)[0].name] = $(this)[0].id;
 });
 
 $(window).resize(function() {
-    let timer;
-    if (timer !== false) {
-        clearTimeout(timer);
-    }
-    timer = setTimeout(function() {
-        ini.funcMacy();
-    }, 200);
+  let timer;
+  if(timer !== false) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(function() {
+    ini.funcMacy();
+  }, 200);
 });
+
+// 設定変更時 バックグラウンド更新
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request.type === 'reload') {
+//         location.reload(true)
+//     }
+// });
--- js/option.js
+++ js/option.js
@@ -1,108 +1,121 @@
 let ev = new class {
-    constructor() {
-        //Default settings
-        this.settings = {
-                "toggle": {
-                    "tgglIcon": -1,
-                    "tgglOpenTab": -1
-                },
-                "text": {
-                    "txtScale": ""
-                },
-                "radio": {
-                    "theme": "tmLight"
-                }
-            },
-            this.initData()
+  constructor() {
+    //Default settings
+    this.settings = {
+      "toggle": {"tgglIcon": -1,"tgglOpenTab": -1},
+      "text": { "txtScale": "", "range":{ "sliderLower": "0", "sliderUpper": "0"}},
+      "radio": {"theme": "tmLight"
+      }
+    },
+    this.initData()
+  }
+  // save
+  saveData() {
+    chrome.storage.local.set({ settings: this.settings });
+    this.toast();
+    // 設定変更時 バックグラウンド更新
+    // chrome.runtime.sendMessage({type: 'reload'}, function(response) {});
+  }
+  // load
+  initData() {
+    chrome.storage.local.get((a) => {
+      a.settings === undefined ? (this.saveData(), this.init()) : (this.settings = a.settings, this.init())
+    })
+  }
+  // called initData()
+  // Initialization
+  init() {
+    this.walkJson(this.settings)
+    this.pageFunc()
+    this.versionInfo()
+    this.gitCommitsInfo()
+    rippleEffect();
+  }
+  // called init() 
+  walkJson(data) {
+    for(var key in data) {
+      if(typeof data[key] === "object") {
+        this.setState(key, data[key])
+      }
     }
-    // save
-    saveData() {
-        chrome.storage.local.set({settings: this.settings});
-        this.toast();
-    }
-    // load
-    initData() {
-        chrome.storage.local.get((a) => {
-            a.settings === undefined ? (this.saveData(), this.init()) : (this.settings = a.settings, this.init())
-        })
-    }
-    // called initData()
-    // Initialization
-    init() {
-        this.walkJson(this.settings)
-        this.pageFunc()
-        this.versionInfo()
-        this.gitCommitsInfo()
-        rippleEffect();
-    }
-    // called init() 
-    walkJson(data) {
-        for (var key in data) {
-            if (typeof data[key] === "object") {
-                this.setState(key, data[key])
-            }
+  }
+  // called walkJson()
+  setState(type, data) {
+    for(const key in data) {
+      if(type === 'toggle' && data[key] === 1) {
+        $('#' + key).addClass('toggle_on');
+      } else if(type === 'text') {
+        if(key === 'range') {
+        for(const a in data[key]){
+          $('#' + a).val([data[key][a]]);
+          $('#' + a + 'Range').val([data[key][a]]);
         }
+      }else{
+        $('#' + key).val(data[key]);
+      }
+      } else if(type === 'radio') {
+        $('input[name="' + key + '"]').val([data[key]]);
+      }
+      
     }
-    // called walkJson()
-    setState(type, data) {
-        for (const key in data) {
-            if (type === 'toggle' && data[key] === 1) {
-                $('#' + key).addClass('toggle_on');
-            } else if (type === 'text') {
-                $('#' + key).val(data[key]);
-            } else if (type === 'radio') {
-                $('input[name="' + key + '"]').val([data[key]]);
-            }
+  }
+  // called init()
+  pageFunc() {
+    $('.toggle').click(function() {
+      $(this).toggleClass('toggle_on');
+      ev.settings.toggle[$(this)[0].id] *= -1;
+      ev.saveData();
+    });
+    $('#txtScale').blur(function() {
+      ev.settings.text[$(this)[0].id] = $(this)[0].value;
+      ev.saveData();
+    });
+    $('input[type="radio"]').click(function() {
+      ev.settings.radio[$(this)[0].name] = $(this)[0].id;
+      ev.saveData();
+    });
+    $('input[type="range"]').change(function() {
+      ev.settings.text.range[$(this)[0].name] = $(this)[0].value;
+      $('#' + $(this)[0].name).val($(this)[0].value);
+      ev.saveData();
+    });
+    $('.textTime').blur(function() {
+      let id = $(this)[0].id;
+      $('#' + id + 'Range').val($(this)[0].value);
+      ev.settings.text.range[$(this)[0].id] = $(this)[0].value;
+      ev.saveData();
+    });
+  }
+
+  versionInfo() {
+    $.getJSON('manifest.json').then(function(manifest) {
+      let str = '<div class="cardContents"><b>Installed Version</b><br>' + manifest.version + '</div>';
+      $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/releases/latest').then(function(data) {
+        if(manifest.version !== data.name) {
+          str += '<h2>#Latest Release</h2><div class="cardContents"><b>Version</b><br>' +
+            data.name + '</div><div class="cardContents"><b>What\'s New</b><br>' +
+            data.body + '</div><div class="cardContents"><b>URL</b><br><a href="' + data.html_url + '"></a></div>';
+          str = str.replace(/\r?\n/g, '<br>');
         }
-    }
-    // called init()
-    pageFunc() {
-        $('.toggle').click(function() {
-            $(this).toggleClass('toggle_on');
-            ev.settings.toggle[$(this)[0].id] *= -1;
-            ev.saveData();
-        });
-        $('.textf').blur(function() {
-            ev.settings.text[$(this)[0].id] = $(this)[0].value;
-            ev.saveData();
-        });
-        $('input[type="radio"]').click(function() {
-            ev.settings.radio[$(this)[0].name] = $(this)[0].id;
-            ev.saveData();
-        });
-    }
+        $('#ExtensionInfo').append(str);
+      });
+    });
+  }
 
-    versionInfo() {
-        $.getJSON('manifest.json').then(function(manifest) {
-            let str = '<div class="cardContents"><b>Installed Version</b><br>' + manifest.version + '</div>';
-            $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/releases/latest').then(function(data) {
-                if (manifest.version !== data.name) {
-                    str += '<h2>#Latest Release</h2><div class="cardContents"><b>Version</b><br>' +
-                        data.name + '</div><div class="cardContents"><b>What\'s New</b><br>' +
-                        data.body + '</div><div class="cardContents"><b>URL</b><br><a href="' + data.html_url + '"></a></div>';
-                    str = str.replace(/\r?\n/g, '<br>');
-                }
-                $('#ExtensionInfo').append(str);
-            });
-        });
-    }
+  gitCommitsInfo() {
+    let str = '';
+    $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/commits').then(function(json) {
+      for(let i = 0; i < 5; i++) {
+        str += '<div class="cardContents"><b>' + json[i].commit.message + '</b><br><span>' +
+          (json[i].commit.author.date).replace('T', ', ').slice(0, -1) +
+          ' (UTC)</span><br><a href="' + json[i].html_url + '"></a></div>';
+      }
+      $('#gitCommitsInfo').append(str);
+    });
+  }
 
-    gitCommitsInfo() {
-        let str = '';
-        $.getJSON('https://api.github.com/repos/Yoseatlly/HelloNewTab/commits').then(function(json) {
-            for (let i = 0; i < 5; i++) {
-                str += '<div class="cardContents"><b>' + json[i].commit.message + '</b><br><span>' +
-                    (json[i].commit.author.date).replace('T', ', ').slice(0, -1) +
-                    ' (UTC)</span><br><a href="' + json[i].html_url + '"></a></div>';
-            }
-            $('#gitCommitsInfo').append(str);
-        });
-    }
-
-    toast() {
-        $('#toast').css('bottom', '1rem');
-        // setTimeout("this.toasted()", 1500);
-        setTimeout(function() {$('#toast').css('bottom', '-5rem');}, 3000);
-    }
+  toast() {
+    $('#toast').css('bottom', '1rem');
+    setTimeout(function() { $('#toast').css('bottom', '-5rem'); }, 3000);
+  }
 }
-
